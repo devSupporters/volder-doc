4453dfbcd64ec38da15d07d76b129084cd85dfbb
salah alhashmi <alhashmis246@gmail.com>
2022-01-27 20:25:14 +0400

    feat: add get started page full of content

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,7 +48,7 @@ const config = {
           },
           { to: "/blog", label: "Blog", position: "left" },
           {
-            href: "https://github.com/facebook/docusaurus",
+            href: "https://github.com/devSupporters/volder",
             label: "GitHub",
             position: "right",
           },
